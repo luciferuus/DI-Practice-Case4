a86494d96606cdb0d409168619d73046d50d5f32
Your Name <luciferuus@yandex.ru>
2024-03-02 09:53:00 +0700

    Initial commit
    Just me uploading everything I've got

---
+++ data_restore.py
@@ -0,0 +1,5 @@
+import shutil
+
+shutil.copyfile('./Data/Original/fourth_case_activity.csv', './Data/Production/fourth_case_activity.csv')
+shutil.copyfile('./Data/Original/fourth_case_demographics.csv', './Data/Production/fourth_case_demographics.csv')
+shutil.copyfile('./Data/Original/fourth_case_purchase.csv', './Data/Production/fourth_case_purchase.csv')
